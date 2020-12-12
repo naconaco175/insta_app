f29a34d35e9c0224fa632f157c7b39adda67b1b6
hinako.uchida <nikoniko73.0107@gmail.com>
2020-12-12 08:04:08 +0000

    Add fulltitleHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  #ページごとの完全なタイトルを返します
+  def full_title(page_titel = '')
+    base_title = "Instagram App"
+    if page_titel.empty?
+      base_title
+    else
+      page_titel + " | " + base_title
+    end
+  end
+
 end
