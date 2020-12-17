7e9b7dd5d96d6ede4ef1602e257ccd96d63e12fc
naconaco175 <nikoniko73.0107@gmail.com>
2020-12-17 22:44:13 +0000

    set username & email on git

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   
   root 'static_pages#home'
-  get '/service_rule', to:  'static_pages#service_rule'
-  get '/contact',      to:  'static_pages#contact'
+  get '/singup',       to: 'users#new'
+  get '/service_rule', to: 'static_pages#service_rule'
+  get '/contact',      to: 'static_pages#contact'
   
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -7,7 +7,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 3
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", singup_path
     get contact_path
     assert_select "title", full_title("Contact")
+    get singup_path
+    assert_select "title", full_title("Sign up")
   end
 end
