17acfbd4acb92ec93450653240e8803deec7c23a
naconaco175 <nikoniko73.0107@gmail.com>
2020-12-18 20:27:16 +0000

    rere-create Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/service_rule', to:  'static_pages#service_rule'
   get '/contact',      to:  'static_pages#contact'
-  
+  get '/signup',      to:  'users#new'
 end
