b7cb08912b37922a6a329b9e1e0187bfe220a518
naconaco175 <nikoniko73.0107@gmail.com>
2021-01-02 13:27:59 +0000

    chang image layout & Add service rule page

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -12,6 +12,6 @@ class Micropost < ApplicationRecord
                                       
   # 表示用のリサイズ済み画像を返す
   def display_image
-    image.variant(resize_to_limit: [500, 500])
+    image.variant(resize_to_fill: [200, 200])
   end
 end
